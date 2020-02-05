d888ae401a68dc11f8014a3f4d27326bd28a6324
Tegan <tegan478@gmail.com>
2020-02-05 13:00:36 -0600

    Attempts at fixing visibility
    failed!

--- CS292-Template/Assets/Scripts/Spawner.cs
+++ CS292-Template/Assets/Scripts/Spawner.cs
@@ -14,7 +14,6 @@ public class Spawner : MonoBehaviour
     public float minTimeBtwSpawns;
     public float decrease;
 
-    // Update is called once per frame
     void Update()
     {
         if (timeBtwSpawns <= 0)
@@ -22,7 +21,13 @@ public class Spawner : MonoBehaviour
             Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
             GameObject randomItem = items[Random.Range(0, items.Length)];
 
-            Instantiate(randomItem, randomSpawnPoint.position, Quaternion.Euler(0, 180, 0));
+            Instantiate(randomItem, randomSpawnPoint.position, Quaternion.identity);
+
+            Rigidbody2D rigidFood = Instantiate(randomItem.GetComponent<Rigidbody2D>());
+            rigidFood.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            rigidFood.transform.localScale = new Vector2(1.0f, 1.0f);
+            rigidFood.AddForce(rigidFood.transform.forward * 30.0f);
+            rigidFood.velocity = Vector3.up * 70;
 
             if (startTimeBtwSpawns > minTimeBtwSpawns)
             {
