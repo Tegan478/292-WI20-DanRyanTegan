c10a6e52c046aec2613547b4f1c9b3ecdeaef0aa
Ryan Earles <rjearles@knox.edu>
2020-02-20 17:58:18 -0600

    SFX Button Mute

--- CS292-Template/Assets/Scripts/ButtonScripts/SFXButton.cs
+++ CS292-Template/Assets/Scripts/ButtonScripts/SFXButton.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SFXButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public void SFXDisable()
+{
+
+   AudioListener.pause = !AudioListener.pause;
+
 }
+
+ 
+}
+
+
