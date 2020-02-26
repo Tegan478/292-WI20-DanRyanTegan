88920b3bc549568d85de5af0c392e634a6ec5d06
Ryan Earles <rjearles@knox.edu>
2020-02-26 11:45:03 -0600

    The SFX Button works
    The SFX button mutes the sound on the prefabs of the particles and mutes the button audiosource

--- CS292-Template/Assets/Scripts/ButtonScripts/SFXButton.cs
+++ CS292-Template/Assets/Scripts/ButtonScripts/SFXButton.cs
@@ -2,11 +2,19 @@
 
 public class SFXButton : MonoBehaviour
 {
+    public GameObject[] items;
+    public AudioSource button;
+
 
     public void SFXDisable()
     {
-
-        AudioListener.pause = !AudioListener.pause;
+        button.mute = !button.mute;
+        foreach (GameObject item in items)
+       {
+            item.GetComponent<AudioSource>().mute = !item.GetComponent<AudioSource>().mute;
+       }
+        
+        
 
     }
 
